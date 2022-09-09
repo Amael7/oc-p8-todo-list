c5fb6ed637bed42061004a08c200b6ebd477a7a1
M.Stéphane <stephane.montoro@hotmail.com>
2022-09-09 21:31:38 +0200

    clean

--- src/AppBundle/Form/TaskType.php
+++ src/AppBundle/Form/TaskType.php
@@ -12,7 +12,7 @@ class TaskType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('content', TextareaType::class)
+            ->add('content', TextareaType::class)  
             //->add('author') ===> must be the user authenticated
         ;
     }
