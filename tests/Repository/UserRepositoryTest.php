df212cda34ade6be6dde9fc8b3decab50f6e7b9f
M.Stéphane <stephane.montoro@hotmail.com>
2022-10-28 12:46:33 +0200

    Update UserRepositoryTest.php

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -2,13 +2,13 @@
 
 namespace App\tests\Repository;
 
-use App\DataFixtures\TaskTestFixtures;
-use App\DataFixtures\UserTestFixtures;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 class UserRepositoryTest extends KernelTestCase
 {
   public function testUserFixturesNumber()
   {
+    $users = static::getContainer()->get(UserRepository::class)->count([]);
+    $this->assertSame(11, $users);
   }
 }
