51d6b1cfa9023e82281255ff3151914656858d1a
M.Stéphane <stephane.montoro@hotmail.com>
2022-10-28 14:09:37 +0200

    Update LoginControllerTest.php

--- tests/Controller/LoginControllerTest.php
+++ tests/Controller/LoginControllerTest.php
@@ -2,13 +2,36 @@
 
 namespace App\tests\Controller;
 
-use App\Tests\Utils\NeedLogin;
-use App\DataFixtures\TaskTestFixtures;
-use App\DataFixtures\UserTestFixtures;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LoginControllerTest extends WebTestCase
 {
-    
+  private $client = null;
+
+  public function setUp(): void
+  {
+    $this->client = static::createClient();
+    $this->userRepository = $this->client->getContainer()->get('doctrine.orm.entity_manager')->getRepository(User::class);
+    $this->user = $this->userRepository->findOneByEmail('user1@hotmail.com');
+    $this->urlGenerator = $this->client->getContainer()->get('router.default');
+  }
+
+  /**
+   * Test login page not authenticated user.
+   */
+  public function testLoginPage(): void
+  {
+    $crawler = $this->client->request(Request::METHOD_GET, $this->urlGenerator->generate('login'));
+    $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    $this->assertSelectorExists('form');
+    $this->assertSame(1, $crawler->filter('html:contains("Nom d\'utilisateur :")')->count());
+    $this->assertSame(1, $crawler->filter('html:contains("Mot de passe :")')->count());
+    $this->assertCount(3, $crawler->filter('input'));
+    $this->assertSelectorTextSame('button', 'Se connecter');
+  }
+
+  
 }
