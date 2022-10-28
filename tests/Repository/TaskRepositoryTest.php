f609870af74e1afb01dc02eaa800ebebc71ca000
M.Stéphane <stephane.montoro@hotmail.com>
2022-10-28 12:46:36 +0200

    Update TaskRepositoryTest.php

--- tests/Repository/TaskRepositoryTest.php
+++ tests/Repository/TaskRepositoryTest.php
@@ -2,12 +2,14 @@
 
 namespace App\tests\Repository;
 
-use App\DataFixtures\TaskFixtures;
-use App\DataFixtures\UserFixtures;
 use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class TaskRepositoryTest extends KernelTestCase
 {
-
+  public function testTaskFixturesNumber()
+  {
+    $tasks = static::getContainer()->get(TaskRepository::class)->count([]);
+    $this->assertSame(10, $tasks);
+  }
 }
