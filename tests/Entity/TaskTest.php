8ab24eb021c5ee323a45850446d3cc1c99c0d139
M.Stéphane <stephane.montoro@hotmail.com>
2022-10-29 13:43:12 +0200

    Update TaskTest.php

--- tests/Entity/TaskTest.php
+++ tests/Entity/TaskTest.php
@@ -3,6 +3,7 @@
 namespace App\tests\Entity;
 
 use App\Entity\Task;
+use DateTimeImmutable;
 use App\Tests\Tools\AssertHasErrors;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -15,11 +16,11 @@ class TaskTest extends KernelTestCase
    */
   public function getEntity(): Task
   {
-      $task = new Task();
-      $task->setTitle('title');
-      $task->setContent('content');
+    $task = new Task();
+    $task->setTitle('title');
+    $task->setContent('content');
 
-      return $task;
+    return $task;
   }
 
   /**
@@ -35,10 +36,10 @@ class TaskTest extends KernelTestCase
    */
   public function testInvalidBlankEntity(): void
   {
-      $task = $this->getEntity();
-      $task->setTitle('');
-      $task->setContent('');
-      $this->assertHasErrors($task, 2);
+    $task = $this->getEntity();
+    $task->setTitle('');
+    $task->setContent('');
+    $this->assertHasErrors($task, 2);
   }
 
   /**
@@ -46,9 +47,9 @@ class TaskTest extends KernelTestCase
    */
   public function testToggleDone(): void
   {
-      $task = $this->getEntity();
-      $task->toggle(true);
-      $this->assertTrue($task->isIsDone());
+    $task = $this->getEntity();
+    $task->toggle(true);
+    $this->assertTrue($task->isIsDone());
   }
 
   /**
@@ -56,8 +57,28 @@ class TaskTest extends KernelTestCase
    */
   public function testToggleFalse(): void
   {
-      $task = $this->getEntity();
-      $task->toggle(false);
-      $this->assertFalse($task->isIsDone());
+    $task = $this->getEntity();
+    $task->toggle(false);
+    $this->assertFalse($task->isIsDone());
+  }
+
+  /**
+   * Check toggle task entity.
+   */
+  public function testSetIsDone(): void
+  {
+    $task = $this->getEntity();
+    $task->SetIsDone(true);
+    $this->assertTrue($task->isIsDone());
+  }
+
+  /**
+   * Check toggle task entity.
+   */
+  public function testCreatedAt(): void
+  {
+    $task = $this->getEntity();
+    $task->setCreatedAt(new \DateTimeImmutable());
+    $this->assertInstanceOf(DateTimeImmutable::class, $task->getCreatedAt());
   }
 }
